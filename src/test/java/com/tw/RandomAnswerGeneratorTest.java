0bc98d51bfd7d952f82addceecd60081420c62e0
Yangyi Li <yangyli@thoughtworks.com>
2020-02-07 12:27:12 +0800

    add test case answer has no duplicate number and answer is random

--- src/test/java/com/tw/RandomAnswerGeneratorTest.java
+++ src/test/java/com/tw/RandomAnswerGeneratorTest.java
@@ -2,19 +2,37 @@ package com.tw;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RandomAnswerGeneratorTest {
+
     @Test
     public void should_return_4_digit_string() {
         RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
         String answer = randomAnswerGenerator.generate();
         Assert.assertEquals(4, answer.length());
-        // StringUtil
+        Assert.assertEquals(true, StringUtils.isNumeric(answer));
     }
 
     @Test
     public void should_no_duplicate() {
         RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
+        String answer = randomAnswerGenerator.generate();
+        Set answer_hash = new HashSet<String>(Arrays.asList(answer.split("")));
+        Assert.assertTrue(answer_hash.size() == answer.length());
+    }
+
+    @Test
+    public void should_random() {
+        RandomAnswerGenerator randomAnswerGenerator = new RandomAnswerGenerator();
+        String answer_1 = randomAnswerGenerator.generate();
+        String answer_2 = randomAnswerGenerator.generate();
+        Assert.assertFalse(answer_1.equals(answer_2));
     }
 
 }
