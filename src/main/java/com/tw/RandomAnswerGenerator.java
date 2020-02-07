e9271f0c6e3acf737cf084202f75672461b1fcd6
Yangyi Li <yangyli@thoughtworks.com>
2020-02-07 12:49:41 +0800

    implement generate random number string

--- src/main/java/com/tw/RandomAnswerGenerator.java
+++ src/main/java/com/tw/RandomAnswerGenerator.java
@@ -1,7 +1,18 @@
 package com.tw;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class RandomAnswerGenerator implements AnswerGenerator {
     public String generate() {
-        return "1234";
+        Set answer = new HashSet<String>();
+        Random random = new Random();
+        while (answer.size() < 4) {
+            answer.add(random.nextInt(10));
+        }
+        return StringUtils.join(answer.toArray(), "");
     }
 }
--- src/test/java/com/tw/RandomAnswerGeneratorTest.java
+++ src/test/java/com/tw/RandomAnswerGeneratorTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.apache.commons.lang3.StringUtils;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
